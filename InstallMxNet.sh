28a21957bacc0352f048cce13f79d6aa637fbdb7
YANG PIN CHU <yang.pinchu@jp.panasonic.com>
2017-12-07 10:43:46 +0900

    Fix mxnet installation scripts bug

--- InstallMxNet.sh
+++ InstallMxNet.sh
@@ -1,4 +1,5 @@
-installation_folder="pwd"
+installation_folder=$(pwd)
+cd $HOME
 
 sudo apt-get update
 sudo apt-get install -y wget python
@@ -8,21 +9,23 @@ sudo apt-get install -y libopenblas-dev liblapack-dev
 
 #Clone the source code
 git clone --recursive https://github.com/apache/incubator-mxnet.git mxnet --branch 0.11.0
-cd mxnet/
+cd $HOME/mxnet/
+MXNET_ROOT=$HOME/mxnet/
+
 
 #Replace the config file that cannot work
-rm ./make/config.mk
-cp $installation_folder/mxnet0_11_JetsonTX1/config.mk .
-cp $installation_folder/mxnet0_11_JetsonTX1/config.mk ./make/
-rm ./mshadow/make/mshadow.mk
-cp $installation_folder/mxnet0_11_JetsonTX1/mshadow.mk ./mshadow/make/
+rm $MXNET_ROOT/make/config.mk
+cp $installation_folder/mxnet0_11_JetsonTX1/config.mk $MXNET_ROOT
+cp $installation_folder/mxnet0_11_JetsonTX1/config.mk $MXNET_ROOT/make/
+rm $MXNET_ROOT/mshadow/make/mshadow.mk
+cp $installation_folder/mxnet0_11_JetsonTX1/mshadow.mk $MXNET_ROOT/mshadow/make/
 
 #Make and install
 make -j $(nproc) USE_OPENCV=1 USE_BLAS=openblas USE_CUDA=1 USE_CUDA_PATH=/usr/local/cuda USE_CUDNN=1
 
 #Install the MXNet python binding
 sudo apt-get install -y python-dev python-setuptools python-numpy python-pip
-cd python
+cd $MXNET_ROOT/python
 sudo pip install --upgrade pip
 sudo pip install -e .
 
